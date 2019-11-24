544af03c1222a3a27017b582b5ab6d85ac7b79aa
LeoPFreitas <leonardofreitasdev@outlook.com>
2019-11-24 16:39:17 -0300

    create simple server routes

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+
 const server = express()
+
+// para utililizar arquivos estáticos
+server.use(express.static('public'))
+
+// configurar template engine
+server.set("view engine", 'html')
+
+nunjucks.configure("views", {
+    express: server
+})
 
 // criando a rota
 server.get("/", function (req, res) {
-    return res.send('Deu bom')
+    return res.render("index")
 })
 
+server.get("/receitas", function (req, res) {
+    return res.render("receitas")
+})
+
+server.get("/sobre", function (req, res) {
+    return res.render("sobre")
+})
 
 server.listen(5000, function () {
     console.log("Server is running!")
