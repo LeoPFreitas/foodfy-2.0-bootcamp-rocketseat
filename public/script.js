2ddbfa62dabd2e303be5e7e6fb1abd6024229ecc
LeoPFreitas <leonardofreitasdev@outlook.com>
2019-11-25 16:10:31 -0300

    Show/Hide func

--- public/script.js
+++ public/script.js
@@ -1,9 +1,24 @@
 const recipes = document.querySelectorAll('.recipe')
+const buttons = document.getElementsByClassName('button')
 
 for (let recipe of recipes) {
     recipe.addEventListener("click", function () {
         const recipeId = recipe.getAttribute('id')
 
         window.location.href = `receitas/${recipeId}`
+    })
+}
+
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        if (button.textContent == "Esconder") {
+            button.textContent = "Mostrar"
+        } else {
+            button.textContent = "Esconder"
+        }
+
+        const idName = button.getAttribute('id')
+        const content = document.querySelector(`.${idName}-content`)
+        content.classList.toggle('hide')
     })
 }
